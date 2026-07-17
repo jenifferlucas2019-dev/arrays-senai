1e9776e7af7b44a2d5e97ebdb214f5804bae5433
Jeniffer schurhaus Lucas <jenifferlucas2019@gmail.com>
2026-07-16 21:39:38 -0300

    Add files via upload

---
+++ 09-arry/script.js
@@ -0,0 +1,283 @@
+// const personagens = ["Lúcio Fernando", "Mônica", "Capitão Ganso"];
+// console.log(personagens);
+// const novoPersonagens = personagens.push("gill bates");
+// console.log(novoPersonagens);
+// const novoersonagens = personagens.push("Dona Bete");
+// console.log(novoersonagens);
+
+// const Personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates"
+// ];
+// Personagens.pop(3)
+//   console.log(Personagens);
+
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates"
+// ];
+//  personagens.shift(0)
+//  console.log(personagens);
+ 
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates",
+//     "Junin"
+// ];
+// const indice = personagens.indexOf("Capitão Ganso");
+
+// personagens.splice(indice, 1);
+
+// console.log(personagens);
+
+
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates",
+//     "Junin"
+// ];
+
+// const indice = personagens.indexOf("Gill Bates","Junin" )
+// personagens.splice(indice, 4, 5);
+// console.log(personagens);
+
+
+// const personagens = [
+//     "Peba",
+//     "Bilu",
+//     "Waldisney"
+// ];
+
+// personagens.push("Padre Ernan Buco");
+// console.log(personagens);
+
+
+// const personagens = [
+//     "Peba",
+//     "Bilu",
+//     "Waldisney"
+// ];
+
+//  personagens.push("GENéZio","Kowalski");
+//  console.log(personagens);
+ 
+
+// const personagens = [
+//     "Peba",
+//     "Bilu",
+//     "Waldisney",
+//     "GENéZio"
+// ];
+// const indice = personagens.indexOf("Waldisney");
+
+// console.log(indice); 
+
+
+// const personagens = [
+//     "Peba",
+//     "Bilu",
+//     "Waldisney",
+//     "GENéZio"
+// ];
+
+// console.log(personagens[0]); 
+// console.log(personagens[1]); 
+// console.log(personagens[2]); 
+// console.log(personagens[3]); 
+
+// personagens.forEach((personagem) => {
+//     console.log(personagem);
+// });
+
+
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates"
+// ];
+// personagens.reverse()
+// console.log(personagens);
+
+
+// const personagens = [
+//     "Tião",
+//     "Junin",
+//     "Padre Ernan Buco"
+// ];
+// personagens. splice(1)
+// console.log(personagens);
+
+
+// const personagens = [
+//     "Tião",
+//     "Junin",
+//     "Padre Ernan Buco"
+// ];
+// personagens.push("Dona Bete")
+// personagens.unshift( "Mônica")
+// console.log(personagens);
+
+
+// const personagens = [
+//     "Rivaldo Jesus",
+//     "Kowalski",
+//     "GENéZio",
+//     "Waldisney"
+// ];
+// personagens.forEach((personagem) => {
+//     console.log(personagem);
+// });
+// console.log(personagens[0]); 
+// console.log(personagens[1]); 
+
+
+// const personagens = [
+//     "Rivaldo Jesus",
+//     "Kowalski",
+//     "GENéZio",
+//     "Waldisney"
+// ];
+// console.log(personagens[0]); 
+// personagens.forEach(personagem => {
+//     console.log(personagem);
+//     const aleatorio = personagens[Math.floor(Math.random() * personagens.length)];
+// console.log(aleatorio);
+// });
+
+// const personagens = [
+//     "Rivaldo Jesus",
+//     "Kowalski",
+//     "GENéZio",
+//     "Waldisney"
+// ];
+// const indece=personagens.indexOf("genézio");
+// console.log("índece de genézio",indece);
+// const fazparte=personagens.includes("capitao ganso");
+// if(fazparte){
+//     console.log("capitao ganso faz parte do array");
+// }else{
+// console.log("capitao ganso  nao faz parte do array");
+
+// }
+    
+// const personagens = [
+//     "Bilu",
+//     "Mônica",
+//     "Gill Bates",
+//     "Junin",
+//     "Peba"
+// ];
+
+// personagens.shift("Bilu")
+// personagens.shift("Mônica")
+// console.log(personagens);
+
+// const personagens = [
+//     "Bilu",
+//     "Mônica",
+//     "Gill Bates",
+//     "Junin",
+//     "Peba"
+// ];
+// personagens.pop("Junin")
+// personagens.pop( "Peba")
+// console.log(personagens);
+
+// const personagens = [
+//     "Capitão Ganso",
+//     "Heitor Tuga",
+//     "Dona Bete"
+// ];
+// personagens.unshift("Lúcio Fernando");
+// personagens.push("Gill Bates");
+
+// console.log(personagens);
+
+// const personagens = [
+//     "Capitão Ganso",
+//     "Heitor Tuga",
+//     "Dona Bete",
+//     "Gill Bates"
+// ];
+// personagens.reverse()
+// console.log(personagens);
+
+// const personagens = [
+//     "Padre Ernan Buco",
+//     "GENéZio",
+//     "Bilu",
+//     "Junin"
+// ];
+// personagens.includes("Bilu")
+// console.log(personagens);
+
+// const personagens = [
+//     "Padre Ernan Buco",
+//     "GENéZio",
+//     "Bilu",
+//     "Junin"
+// ];
+// personagens.indexOf("Padre Ernan Buco")
+// console.log(personagens);
+
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Rivaldo Jesus",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates",
+//     "Junin"
+// ];
+// personagens.splice(2, 2)
+// console.log(personagens);
+
+// const personagens = [
+//     "Tião",
+//     "Peba",
+//     "Waldisney"
+// ];
+// personagens.push("Bilu")
+// console.log(personagens);
+// personagens.splice(0,0,"Dona Bete")
+// console.log(personagens);
+// personagens.pop()
+// console.log(personagens);
+// personagens.reverse()
+// console.log(personagens);
+
+// const personagens = [
+//     "Gill Bates",
+//     "Kowalski",
+//     "GENéZio",
+//     "Padre Ernan Buco",
+//     "Heitor Tuga"
+// ];
+
+// const existeKowalski = personagens.includes("Kowalski");
+// console.log(existeKowalski); 
+
+
+// const indicePadre = personagens.indexOf("Padre Ernan Buco");
+// console.log(indicePadre); 
+
+
+// const indiceGenezio = personagens.indexOf("GENéZio");
+// if (indiceGenezio !== -1) {
+//     personagens.splice(indiceGenezio, 1);
+// }
+
+// console.log(personagens);
+
+
+// personagens.reverse();
+
+// console.log(personagens);
